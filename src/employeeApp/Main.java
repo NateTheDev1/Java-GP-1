ed113ff225c3a1321799e54e9162b57f4db3a224
NateTheDev1 <nthnlrichards@gmail.com>
2020-06-25 02:29:16 -0400

    First class and jar

---
+++ src/employeeApp/Main.java
@@ -0,0 +1,10 @@
+package employeeApp;
+
+public class Main {
+    private static void workWithData() {
+        System.out.println("Welcome to Lambda School's Java Unit!");
+    }
+   public static void main(String[] args) {
+       workWithData();
+   }
+}
